47eeb2dbbf3a37820d392d401a2d6028df123c26
Stephen Onnen <stephen.onnen@gmail.com>
2021-10-02 18:38:42 +0000

    Added dataclass

--- py_check/main.py
+++ py_check/main.py
@@ -1,9 +1,18 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class FooBar:
+    foo: str = "foo"
+    bar: str = "baz"
+
+
 def get_foo() -> str:
-    return "foo"
+    return FooBar.foo
 
 
 def get_bar() -> str:
-    return "baz"
+    return FooBar.bar
 
 
 if __name__ == "__main__":
